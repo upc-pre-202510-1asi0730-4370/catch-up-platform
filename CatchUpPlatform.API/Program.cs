d88e3639b8bae0a7666356a0dbe8f88956ada921
Alberto Sanchez S <albertosanchez05@gmail.com>
2025-05-30 20:44:13 -0500

    feat: configure database and connectionstring on Program.cs

--- CatchUpPlatform.API/Program.cs
+++ CatchUpPlatform.API/Program.cs
@@ -1,9 +1,41 @@
+using CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Add Database Connection
+var connectionString = builder.Configuration.GetConnectionString(@"DefaultConnection");
+
+// Verify Database Connection String
+if (connectionString is null)
+    throw new Exception("Connection string is null");
+
+// Configure Database Context and Logging Levels
+if (builder.Environment.IsDevelopment())
+    builder.Services.AddDbContext<AppDbContext>(
+        options =>
+        {
+            options.UseMySQL(connectionString)
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+        }
+    );
+else if (builder.Environment.IsProduction())
+    builder.Services.AddDbContext<AppDbContext>(
+        options =>
+        {
+            options.UseMySQL(connectionString)
+                .LogTo(Console.WriteLine, LogLevel.Error)
+                .EnableDetailedErrors();
+        }
+    );
+    
+    
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
